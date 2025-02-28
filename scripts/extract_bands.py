b9e75205f1e49427a6484b4e4bf27506714ff88f
Katherine <krosenf@gmail.com>
2025-02-27 17:17:36 -0800

    update name

--- scripts/extract_bands.py
+++ scripts/extract_bands.py
@@ -1,3 +1,5 @@
-from ihme_vaccine_rasters import extract_all_bands
-input_raster = "/workspaces/IHME_vaccine_rasters/data/mcv1_cov_mean_raked_2000_2023.tif"
-extract_all_bands(input_raster=input_raster, output_dir="/workspaces/IHME_vaccine_rasters/data")
+import os
+from raster_bands import extract_all_bands
+os.chdir(os.path.dirname(__file__))
+input_raster = "../data/mcv1_cov_mean_raked_2000_2023.tif"
+extract_all_bands(input_raster=input_raster, output_dir="../data")

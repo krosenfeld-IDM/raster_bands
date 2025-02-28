501050b4f58c7dd01e04af3ce1c4d327db28d0d3
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2025-02-28 01:01:22 +0000

    simplify

--- scripts/extract_bands.py
+++ scripts/extract_bands.py
@@ -1,9 +1,3 @@
-from ihme_vaccine_rasters import extract_band, extract_all_bands
-
+from ihme_vaccine_rasters import extract_all_bands
 input_raster = "/workspaces/IHME_vaccine_rasters/data/mcv1_cov_mean_raked_2000_2023.tif"
-output_raster = "/workspaces/IHME_vaccine_rasters/data/mcv1_cov_mean_raked_2000_2023_band1.tif"
-band_number = 1
-
-# extract_band(input_raster=input_raster, output_raster=output_raster, band_number=band_number)
-
 extract_all_bands(input_raster=input_raster, output_dir="/workspaces/IHME_vaccine_rasters/data")
